234bce72aa29364c56e5bf31f8f6e6715d00d740
Swastik Midda <78782223+failure64@users.noreply.github.com>
2023-07-23 12:43:23 +0530

    Update CategoryBox.tsx

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -6,59 +6,67 @@ import React, { useCallback } from 'react'
 import { IconType } from 'react-icons';
 import qs from 'query-string';
 interface CategoryBoxProps {
-    icon: IconType;
-    label: string;
-    selected?: boolean;
-
+  icon: IconType,
+  label: string;
+  selected?: boolean;
 }
 
 const CategoryBox: React.FC<CategoryBoxProps> = ({
-    icon: Icon,
-    label,
-    selected
+  icon: Icon,
+  label,
+  selected,
 }) => {
-    const router = useRouter();
-    const params = useSearchParams();
+  const router = useRouter();
+  const params = useSearchParams();
 
-    const handelClick = useCallback(() => {
-        let currentQuery = {};
-        if(params){
-            currentQuery = qs.parse(params.toString());
-        }
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+    
+    if (params) {
+      currentQuery = qs.parse(params.toString())
+    }
 
-            const updatedQuery: any ={
-                ...currentQuery,
-                category: label
-            }
- 
-            if (params?.get('category') === label){
-                delete updatedQuery.category; 
-            }
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label
+    }
 
-            const url = qs.stringifyUrl({
-                url: '/',
-                query: updatedQuery 
-            }, {skipNull: true});
+    if (params?.get('category') === label) {
+      delete updatedQuery.category;
+    }
 
+    const url = qs.stringifyUrl({
+      url: '/',
+      query: updatedQuery
+    }, { skipNull: true });
 
-            router.push(url);
-        
+    router.push(url);
+  }, [label, router, params]);
 
-    },[label, params, router]);
-  return (
-    <div onClick={handelClick} 
-    className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer
-    ${selected? 'border-b-neutral-800' : 'border-transparent'}
-    ${selected ? 'text-neutral-800' : 'text-neutral-500'}
-    `}>
-        <Icon size={26} />
-        <div className='font-medium text-sm'>
-            {label}
-
-        </div>
-
+  return ( 
+    <div
+      onClick={handleClick}
+      className={`
+        flex 
+        flex-col 
+        items-center 
+        justify-center 
+        gap-2
+        p-3
+        border-b-2
+        hover:text-neutral-800
+        transition
+        cursor-pointer
+        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
+      `}
+    >
+      <Icon size={26} />
+      <div className="font-medium text-sm">
+        {label}
+      </div>
     </div>
-  )
+   );
 }
-
-export default CategoryBox
+ 
+export default CategoryBox;
